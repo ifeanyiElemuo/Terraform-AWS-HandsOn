59d0e27455d748137818e6501cdc66f17e26e8d8
Ifeanyi Darlington Elemuo <new.email@example.com>
2024-03-15 09:03:11 +0000

    -- created output file to practice outputs of configuration resources details

---
+++ outputs.tf
@@ -0,0 +1,21 @@
+# terraform output block
+
+output "greeting" {
+  description = "Print greeting message"
+  value       = "Hello, world!"
+}
+
+output "vpc_id" {
+  description = "Outputs vpc id"
+  value       = aws_vpc.vpc.id
+}
+
+output "public_url" {
+  description = "Public url for web_server"
+  value       = "http://${aws_instance.ubuntu_server.public_ip}:8080/index.html"
+}
+
+output "vpc_information" {
+  description = "VPC information for environment"
+  value       = "Your ${aws_vpc.vpc.tags.Environment} VPC has an ID of ${aws_vpc.vpc.id}"
+}
